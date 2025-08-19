6c57304439d25a2fdc6b67f41b77991f620d72f3
ferenbulbul61 <ferenbulbul@gmail.com>
2025-08-19 18:07:21 +0300

    fixed not null

---
+++ TourBooking.Infrastructure/Migrations/20250819150552_locationdescripnull.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TourBooking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class locationdescripnull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Longitude",
+                table: "Bookings",
+                type: "double",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LocationDescription",
+                table: "Bookings",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Latitude",
+                table: "Bookings",
+                type: "double",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Longitude",
+                table: "Bookings",
+                type: "decimal(65,30)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(double),
+                oldType: "double",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Bookings",
+                keyColumn: "LocationDescription",
+                keyValue: null,
+                column: "LocationDescription",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LocationDescription",
+                table: "Bookings",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Latitude",
+                table: "Bookings",
+                type: "decimal(65,30)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(double),
+                oldType: "double",
+                oldNullable: true);
+        }
+    }
+}
