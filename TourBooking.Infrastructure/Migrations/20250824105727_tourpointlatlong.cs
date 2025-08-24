79fda1461800429260fad6110a9e019f5749e837
ferenbulbul61 <ferenbulbul@gmail.com>
2025-08-24 14:00:05 +0300

    migration

---
+++ TourBooking.Infrastructure/Migrations/20250824105727_tourpointlatlong.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TourBooking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class tourpointlatlong : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Lat",
+                table: "TourPoints",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Long",
+                table: "TourPoints",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Lat",
+                table: "TourPoints");
+
+            migrationBuilder.DropColumn(
+                name: "Long",
+                table: "TourPoints");
+        }
+    }
+}
